21a72d26477b81738f36c25a2cb2e4b18521b2b7
Dung Ho <dungho@yuukiho.local>
2020-03-21 03:09:00 -0600

    update

--- src/app/mci/lib/chart-canvas.ts
+++ src/app/mci/lib/chart-canvas.ts
@@ -1,9 +1,12 @@
-import { ChartCanvasOptions } from './models/chart-canvas-options';
+import { ChartCanvasOptions, getCursorStyle } from './models/chart-canvas-options';
 import { chartCanvasOptionDefaults } from './options/chart-canvas-options-defaults';
+
+import * as d3 from 'd3';
 
 export class ChartCanvas {
 
   private chartCanvasOptions: ChartCanvasOptions;
+  chartCanvasSvg: d3.Selection<SVGSVGElement, unknown, null, undefined>;
 
   constructor(node: HTMLDivElement, options?: any) {
     this.chartCanvasOptions = {
@@ -13,5 +16,45 @@ export class ChartCanvas {
 
     console.log(node);
     console.log(this.chartCanvasOptions);
+
+    const { className, width, height, zIndex, margin } = this.chartCanvasOptions;
+
+    this.chartCanvasSvg = d3.select(node).append('svg')
+      .attr('class', className)
+      .attr('width', width)
+      .attr('height', height)
+      .style('position', 'absolute')
+      .style('z-index', zIndex + 5);
+
+    this.chartCanvasSvg.append('style')
+      .attr('type', 'text/css')
+      .text(getCursorStyle(className));
+
+
+    const defs = this.chartCanvasSvg.append('defs');
+
+    const clipPath = defs.append('clipPath')
+      .attr('id', 'chart-area-clip');
+
+    const dimensions = this.getDimensions();
+
+    clipPath.append('rect')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', dimensions.width)
+      .attr('height', dimensions.height);
+
+    const g = this.chartCanvasSvg.append('g')
+      .attr('transform', `translate(${margin.left + 0.5}, ${margin.top + 0.5})`);
+
+  }
+
+  private getDimensions() {
+    const { height, width, margin } = this.chartCanvasOptions;
+
+    return {
+      height: height - margin.top - margin.bottom,
+      width: width - margin.left - margin.right
+    };
   }
 }
--- src/app/mci/lib/models/chart-canvas-options.ts
+++ src/app/mci/lib/models/chart-canvas-options.ts
@@ -3,9 +3,54 @@ export interface ChartCanvasOptions {
   zIndex: number;
   height: number;
   width: number;
-  margin: {left: number, right: number, top: number, bottom: number};
+  margin: { left: number, right: number, top: number, bottom: number };
   data: any[];
   xAccessor: Function;
   xScale: Function;
   xExtents: any[];
+}
+
+export function getCursorStyle(className: string) {
+  const tooltipStyle = `
+	.${className}-grabbing-cursor {
+		pointer-events: all;
+		cursor: -moz-grabbing;
+		cursor: -webkit-grabbing;
+		cursor: grabbing;
+	}
+	.${className}-crosshair-cursor {
+		pointer-events: all;
+		cursor: crosshair;
+	}
+	.${className}-tooltip-hover {
+		pointer-events: all;
+		cursor: pointer;
+	}
+	.${className}-avoid-interaction {
+		pointer-events: none;
+	}
+	.${className}-enable-interaction {
+		pointer-events: all;
+	}
+	.${className}-tooltip {
+		pointer-events: all;
+		cursor: pointer;
+	}
+	.${className}-default-cursor {
+		cursor: default;
+	}
+	.${className}-move-cursor {
+		cursor: move;
+	}
+	.${className}-pointer-cursor {
+		cursor: pointer;
+	}
+	.${className}-ns-resize-cursor {
+		cursor: ns-resize;
+	}
+	.${className}-ew-resize-cursor {
+		cursor: ew-resize;
+  }`;
+
+  return tooltipStyle;
 }
